3aee68c1403a9ef896ff13299eb387d109874470
Haller Chamorro <haller@example.com>
2026-06-19 18:18:54 -0500

    Configure production settings, requirements.txt, and Procfile for Railway

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn mascotasnet.wsgi --log-file -
--- mascotasnet/settings.py
+++ mascotasnet/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6d%g+f-sl5u!7fknk=+h$4lsge_y5^vu19&2c+@f326a=a88@h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+import os
 
-ALLOWED_HOSTS = []
+DEBUG = os.environ.get('DEBUG', 'True').lower() in ('true', '1', 'yes')
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
@@ -43,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -75,20 +78,38 @@ WSGI_APPLICATION = 'mascotasnet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mascotasnet',
-        'USER': 'root',
-        'PASSWORD': 'user',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
+MYSQL_URL = os.environ.get('MYSQL_URL') or os.environ.get('DATABASE_URL')
+
+if MYSQL_URL:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ.get('MYSQLDATABASE'),
+            'USER': os.environ.get('MYSQLUSER'),
+            'PASSWORD': os.environ.get('MYSQLPASSWORD'),
+            'HOST': os.environ.get('MYSQLHOST'),
+            'PORT': os.environ.get('MYSQLPORT', '3306'),
+            'OPTIONS': {
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+                'charset': 'utf8mb4',
+            }
         }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'mascotasnet',
+            'USER': 'root',
+            'PASSWORD': 'user',
+            'HOST': 'localhost',
+            'PORT': '3306',
+            'OPTIONS': {
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+                'charset': 'utf8mb4',
+            }
+        }
+    }
 
 
 # Password validation
@@ -125,10 +146,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [BASE_DIR / 'templates']
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'templates']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Envio de correos a consola para desarrollo
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
